26c5b14e81bdedea9cac19120adc5204f18ae762
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2018-11-02 11:39:20 +0100

    Initial Commit

---
+++ src/barretta.cpp
@@ -0,0 +1,23 @@
+//
+//  barretta.cpp
+//  006
+//
+//  Created by nicola bertelloni on 02/11/2018.
+//
+
+#include "barretta.hpp"
+
+//--------------------------------------------------------------
+void BarrettaClass::setup(){
+  
+}
+
+//--------------------------------------------------------------
+void BarrettaClass::update(){
+  
+}
+
+//--------------------------------------------------------------
+void BarrettaClass::draw(){
+  ofDrawCircle(0, 0, 8);
+}
---
+++ src/barretta.hpp
@@ -0,0 +1,19 @@
+//
+//  barretta.hpp
+//  006
+//
+//  Created by nicola bertelloni on 02/11/2018.
+//
+
+
+#include "ofMain.h"
+
+class BarrettaClass : public ofBaseApp{
+  
+public:
+  void setup();
+  void update();
+  void draw();
+  
+};
+
